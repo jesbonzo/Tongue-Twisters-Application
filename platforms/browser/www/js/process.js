19be201d1611b92fb722ff596d78d58f1838b00f
jesbonzo <jessybonzo31@gmailcom>
2017-12-03 09:18:21 +0800

    with html audio tag-

---
+++ platforms/browser/www/js/process.js
@@ -0,0 +1,70 @@
+/*var audio = new Audio('peterpiper.mp3');
+audio.play();
+
+document.getElementById("t1").play();
+
+
+/*  function play_single_sound() {
+    document.getElementById('audiotag1').play();
+  }
+>
+
+<audio controls>
+  <source src="audio/bigblackbug.mp3" type="audio/mpeg">
+</audio>
+*/
+
+/*var setTimer = 0;
+function clock(){
+let timer = <h1>{new Date().toLocaleTimeString()}</h1>;
+	ReactDOM.render(timer,document.getElementById('timer'));	
+
+	setTimer = setTimeout(function(){
+		clock();
+	},1000);
+};
+
+function stopClock(){
+	clearInterval(setTimer);
+}
+
+let btn = <div>
+				<div id='timer' className='text'></div>
+				<div id='execute' className='text'></div>
+				<div id='output' className='text'></div>
+				<div id='error' className='text'></div>
+				<div id='commands' className='text'></div>
+				<div id='dbfile' className='text'></div>
+				<div id='savedb' className='text'></div>
+				<button onClick={clock}>Start Clock</button>
+				<button onClick={stopClock}>Stop Clock</button>
+			</div>;
+
+ReactDOM.render(btn,document.getElementById('root'));*/
+
+/*let data = [{
+title: "AAA",
+desc: "AAAAAAAAAA AAAAAAAAAA AAAAAAAAAA AAAAAAAAAA AAAAAAAAAA AAAAAAAAAA AAAAAAAAAA AAAAAAAAAA" 
+},
+{
+title: "BBB",
+desc: "bbbbbbbbbb bbbbbbbbbb bbbbbbbbbb bbbbbbbbbb bbbbbbbbbb bbbbbbbbbb bbbbbbbbbb bbbbbbbbbb" 
+},
+{
+title: "CCC",
+desc: "CCCCCCCCCC CCCCCCCCCC CCCCCCCCCC CCCCCCCCCC CCCCCCCCCC CCCCCCCCCC CCCCCCCCCC CCCCCCCCCC" 
+},
+{
+title: "DDD",
+desc: "DDDDDDDDDD DDDDDDDDDD DDDDDDDDDD DDDDDDDDDD DDDDDDDDDD DDDDDDDDDD DDDDDDDDDD DDDDDDDDDD" 
+}];
+
+let content = "";
+
+for(let loop of data){
+	content += `<div>
+					<h1></h1>
+					<small></small>
+				</div>`;
+}
+ReactDOM.render(input,document.getElementById('root'));*/
